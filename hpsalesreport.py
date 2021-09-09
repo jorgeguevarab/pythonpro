41feb6af2109562bf8c688fb83e62fcb7a02fbd3
Jorge Guevara Bohórquez <jorge.agb@gmail.com>
2021-09-09 18:01:14 -0500

    Checkpoint 090921

---
+++ hpsalesreport.py
@@ -0,0 +1,40 @@
+import pyodbc
+import pandas as pd
+import json
+import os
+
+def read_config(config_file):
+    with open(config_file, "r") as json_file:
+        configs = json.load(json_file)
+    return configs
+    
+
+def db_connection(server, database, user, password):
+    connectionstr = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+ server +';DATABASE='+database+';UID='+user+';PWD=' + password
+    try:
+        conn = pyodbc.connect(connectionstr)
+        
+    except:
+        print('Unknow connection error')
+        
+    return conn
+
+def generate_xls():
+    pass
+
+def run():
+    conf = read_config('config.json')
+    conn = db_connection(database=conf['database'], server=conf['server'], user=conf['user'], password=os.environ['myAccessToken'])
+    
+    fechaini = input('Escriba la fecha de inicio en formato AAAAMMDD ')
+    fechafin = input('Escriba la fecha final en formato AAAAMMDD ')
+    filepath = f'c:\output\POS_AMPLIFY_AM60791_{fechaini}_{fechafin}.xlsx'
+
+    df = pd.read_sql(f'EXEC hpsalesreport {fechaini}, {fechafin}', conn)
+    df.to_excel(filepath, sheet_name='sales', index = False, header = True)
+
+    print('Reporte generado: ' + filepath)
+
+
+if __name__=='__main__':
+    run()
