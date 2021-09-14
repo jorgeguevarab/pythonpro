38973c35585eccab53288ba60a39d6c85784f5fb
Jorge Guevara Bohórquez <jorge.agb@gmail.com>
2021-09-14 17:02:29 -0500

    Updating object Connection

--- Connection.py
+++ Connection.py
@@ -1,1 +1,16 @@
-# Declaring object for database connection and handling datasets.
+# Declaring object for database connection and handling dataset
+
+class Connection:
+    database_name = str;
+    server = str;
+    username = str;
+    password = str;
+    
+    def __init__(self, server, username, password, database_name):
+        self.server = server
+        self.username = username
+        self.password = password
+        self.database_name = database_name
+    
+    def connect(self):
+        pass
